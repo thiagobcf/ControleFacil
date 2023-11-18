2529e4711a793e785f819307c23fb915856cecf1
thiagobcf <thiagobcfonseca@gmail.com>
2023-11-18 12:41:38 -0300

    Controller Usuario

---
+++ src/ControleFacil.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ControleFacil.Api.contract.Usuario;
+using ControleFacil.Api.Damain.services.classes;
+using ControleFacil.Api.Damain.services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleFacil.Api.Controllers
+{
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioService _usuarioService;
+        public UsuarioController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Adicionar(UsuarioRequestContract contrato)
+        {
+            try
+            {
+               return Created("", await _usuarioService.Adicionar(contrato, 0));
+            }
+            catch (Exception ex)
+            {
+                
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Obter()
+        {
+            try
+            {
+               return Ok(await _usuarioService.Obter(0));
+            }
+            catch (Exception ex)
+            {
+                
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Obter(long id)
+        {
+            try
+            {
+               return Ok(await _usuarioService.Obter(id, 0));
+            }
+            catch (Exception ex)
+            {
+                
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Atualizar(long id, UsuarioRequestContract contrato)
+        {
+            try
+            {
+               return Ok(await _usuarioService.Atualizar(id, contrato, 0));
+            }
+            catch (Exception ex)
+            {
+                
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Deletar(long id)
+        {
+            try
+            {
+               await _usuarioService.Inativar(id, 0);
+               return NoContent();
+            }
+            catch (Exception ex)
+            {
+                
+                return Problem(ex.Message);
+            }
+        }
+    }
+}
